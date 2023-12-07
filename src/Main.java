7411b6e30739cf92c260d56493e49f8075805822
Mjk78 <148325996+Mjk78@users.noreply.github.com>
2023-12-07 16:42:09 +0200

    Update Main.java
    uusi otsikko

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Moi kaikki ryhmäläiset");
+		System.out.println("Moikka, käykäähän muokkaamassa kaikki tätä");
 
 	}
 
