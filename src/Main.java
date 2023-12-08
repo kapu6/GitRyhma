390a4b076a3d1b1f62b24d31995406d4422c0488
Mjk78 <148325996+Mjk78@users.noreply.github.com>
2023-12-08 15:46:23 +0200

    Update Main.java
    Kysytään nimi ja palaute viesti muutettu

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Moikka, käykäähän muokkaamassa kaikki tätä");
+		System.out.print("Hei, mikä on nimesi? ");
+
+        	String nimi = scanner.nextLine();
+	
+		
+		System.out.println("Moikka, " + nimi + "! Loistavaa työtä");
 
 	}
 
